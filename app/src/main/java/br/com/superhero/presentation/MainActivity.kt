3e56fee021e7c8a1d7fb4b069a0e6b7a33171275
John Lima <ti.junior@gmail.com>
2022-08-31 07:18:43 -0300

    Configuração do container navigation no projeto

--- app/src/main/java/br/com/superhero/presentation/MainActivity.kt
+++ app/src/main/java/br/com/superhero/presentation/MainActivity.kt
@@ -2,12 +2,29 @@ package br.com.superhero.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import br.com.superhero.R
+import br.com.superhero.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
     }
 }
